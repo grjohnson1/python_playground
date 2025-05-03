4c7f95ee1dc11b99dab2a3deda57e86b01195df0
grjohnson1 <grjohnson@gmail.com>
2025-05-03 09:23:35 -0600

    Update Eliza.py
    Updates and running tests.

--- Eliza.py
+++ Eliza.py
@@ -1,5 +1,54 @@
 import random
 
+def testPatterns():
+    print(matchWildcard('stop', ['stop']))
+    print(matchWildcard('stop', ['stop', 'now']))
+    print(matchWildcard('stop', ['I', 'stop', 'now']))
+    print(matchWildcard('stop', ['I', 'can', 'stop']))
+    print(matchWildcard('stop', ['I', 'can', 'stop', 'now']))
+    print(matchWildcard('stop', ['I', 'can', 'finish', 'now']))
+    print(matchWildcard([0,'stop', 0], ['stop']))
+    print(matchWildcard([0,'stop', 0], ['stop', 'now']))
+    print(matchWildcard([0,'stop', 0], ['I', 'stop', 'now']))
+    print(matchWildcard([0,'stop', 0], ['I', 'can', 'stop']))
+    print(matchWildcard([0,'stop', 0], ['I', 'stop', 'right', 'now']))
+    print(matchWildcard([0,'stop', 0], ['I', 'can', 'finish', 'now']))
+
+def matchWildcard(term, text, soFar=''):
+    if len(text) == 0:
+        return False, '', []
+    if (isinstance(term, str) and text[0] == term) or\
+            (not isinstance(term,str) and text[0] in term):
+        return True, soFar, text
+    return matchWildcard(term, text[1:], soFar + ' ' + text[0])
+
+def matchPattern(pattern, text):
+    matches = []
+    for i in range(len(pattern)):
+        if pattern[i] == 0:
+            if i + 1 == len(pattern):
+                matches.append(''.join(text))
+            else:
+                success, match, text = \
+                    matchWildcard(pattern[i +1], text)
+                if not success:
+                    return False, []
+                else:
+                    matches.append(match.strip())
+        elif len(text) == 0:
+            return False, []
+        elif not isinstance(pattern[i], str):
+            if text[0] in pattern[i]:
+                matches.append(text[0])
+                text = text[1:]
+            else:
+                return False, []
+        elif pattern[i] == text[0]:
+            matches.append(text[0])
+            text = text[1:]
+        else:
+            return False, []
+        return True, matches
 
 def splitClauses(text):
     return map(lambda x: x.strip(' '),
@@ -25,3 +74,7 @@ while True:
     # Process input inside the loop
     for clause in splitClauses(text):
         print(splitWords(clause))
+
+#matchPattern( )
+
+testPatterns( )
